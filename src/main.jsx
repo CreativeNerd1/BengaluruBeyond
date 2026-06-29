0c56cac74e823935c7cfde7e1bc04e62c2ceb536
lakshmi narayana <Lakshmi.Narayana@traceone.com>
2026-06-29 16:16:11 +0530

    - dummy commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+//Lakshmi Narayana S
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
